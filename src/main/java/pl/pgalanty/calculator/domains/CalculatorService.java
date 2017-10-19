9f1eb914b4f1783d2ede1739a68f2091427c975f
pgalanty <pawel.galanty@gmail.com>
2017-10-19 09:44:57 +0200

    clearing

--- src/main/java/pl/pgalanty/calculator/domains/CalculatorService.java
+++ src/main/java/pl/pgalanty/calculator/domains/CalculatorService.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 @Service
 public class CalculatorService {
 
-
+// some comment
     public static final String REG_EXP = "([\\d]+)[+-/*]([\\d]+)";
 
     public BigDecimal calculate(CalculatorDTO calculatorDTO) {
